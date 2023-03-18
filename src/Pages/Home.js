c5f59e8fd40e09a3c770ac4b895fb14ce3c55e58
@Bahatimyk <manenomichael99@gmail.com>
2023-03-18 13:42:00 +0300

    active nav-bar

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import "./Home.css";
+import * as home from "react-bootstrap";
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div>
+      <div className="nav_section">
+        <home.Container className="nav_container">
+          <home.Nav
+            className="justify-content-end"
+            variant="pills"
+            defaultActiveKey="/home"
+            
+          >
+            <home.Nav.Item className="link_items">
+              <home.Nav.Link href="/home">Home</home.Nav.Link>
+            </home.Nav.Item>
+            <home.Nav.Item className="link_items">
+              <home.Nav.Link eventKey="link-1">Who We Are</home.Nav.Link>
+            </home.Nav.Item>
+            <home.Nav.Item className="link_items">
+              <home.Nav.Link eventKey="link-2" >
+                How To Help
+              </home.Nav.Link>
+            </home.Nav.Item>
+            <home.Nav.Item className="link_items">
+              <home.Nav.Link eventKey="link-2" >
+                Contact Us
+              </home.Nav.Link>
+            </home.Nav.Item>
+          </home.Nav>
+        </home.Container>
+      </div>
+
+      <div className="carousel_section"></div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
