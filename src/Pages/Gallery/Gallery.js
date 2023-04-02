f52dd81e9b628df8d4c040bf998dd3853ad2ab8d
@Bahatimyk <manenomichael99@gmail.com>
2023-04-02 22:42:12 +0300

    image responsiveness

--- src/Pages/Gallery/Gallery.js
+++ src/Pages/Gallery/Gallery.js
@@ -54,106 +54,108 @@ function Gallery() {
       </gala.Container>
     <gala.Container className="galacont">
         <gala.Row className="mb-2">
-            <gala.Col lg={3} className="mx-auto mb-3 ">
+            <gala.Col lg={4} xs={10} className="mx-auto mb-3 ">
                 <gala.Image
                 src={i2}
-                className="d-block w-100"
+                className="d-block w-100 imagegala"
                 alt="picture1"
                 fluid
                 />
             </gala.Col>
-            <gala.Col lg={3} className="mx-auto mb-3">
+            <gala.Col lg={4} xs={10} className="mx-auto mb-3">
                 <gala.Image
                 src={i3}
-                className="d-block w-100"
+                className="d-block w-100 imagegala"
                 alt="picture2"
                 fluid
                 />
             </gala.Col>
-            <gala.Col lg={3} className="mx-auto mb-3">
+            <gala.Col lg={4} xs={10} className="mx-auto mb-3">
                 <gala.Image
                 src={i4}
-                className="d-block w-100"
+                className="d-block w-100 imagegala"
                 alt="picture3"
                 fluid
                 />
             </gala.Col>
-            <gala.Col lg={3} className="mx-auto mb-3">
+            <gala.Col lg={4} xs={10} className="mx-auto mb-3">
                 <gala.Image
                 src={i5}
-                className="d-block w-100"
+                className="d-block w-100 imagegala"
                 alt="picture3"
                 fluid
                 />
             </gala.Col>
-        </gala.Row>
-        <gala.Row className="mb-2">
-            <gala.Col lg={3} className="mx-auto mb-3">
-                <gala.Image
-                src={i7}
-                className="d-block w-100"
-                alt="picture1"
-                fluid
-                />
-            </gala.Col>
-            <gala.Col lg={3} className="mx-auto mb-3">
-                <gala.Image
-                src={i8}
-                className="d-block w-100"
-                alt="picture2"
-                fluid
-                />
-            </gala.Col>
-            <gala.Col lg={3} className="mx-auto mb-3">
-                <gala.Image
-                src={i9}
-                className="d-block w-100"
-                alt="picture3"
-                fluid
-                />
-            </gala.Col>
-            <gala.Col lg={3} className="mx-auto mb-3">
-                <gala.Image
-                src={i10}
-                className="d-block w-100"
-                alt="picture3"
-                fluid
-                />
-            </gala.Col>
-        </gala.Row>
-        <gala.Row className="mb-2">
-            <gala.Col lg={3} className="mx-auto mb-3">
-                <gala.Image
-                src={i11}
-                className="d-block w-100"
-                alt="picture1"
-                fluid
-                />
-            </gala.Col>
-            <gala.Col lg={3} className="mx-auto mb-3">
-                <gala.Image
-                src={i12}
-                className="d-block w-100"
-                alt="picture2"
-                fluid
-                />
-            </gala.Col>
-            <gala.Col lg={3} className="mx-auto mb-3">
-                <gala.Image
-                src={i13}
-                className="d-block w-100"
-                alt="picture3"
-                fluid
-                />
-            </gala.Col>
-            <gala.Col lg={3} className="mx-auto mb-3">
+            <gala.Col lg={4} xs={10} className="mx-auto mb-3">
                 <gala.Image
                 src={i6}
-                className="d-block w-100"
+                className="d-block w-100 imagegala"
                 alt="picture3"
                 fluid
                 />
             </gala.Col>
+            <gala.Col lg={4} xs={10} className="mx-auto mb-3">
+                <gala.Image
+                src={i7}
+                className="d-block w-100 imagegala"
+                alt="picture1"
+                fluid
+                />
+            </gala.Col>
+        </gala.Row>
+        <gala.Row className="mb-2">
+            
+            <gala.Col lg={4} xs={10} className="mx-auto mb-3">
+                <gala.Image
+                src={i8}
+                className="d-block w-100 imagegala"
+                alt="picture2"
+                fluid
+                />
+            </gala.Col>
+            <gala.Col lg={4} xs={10} className="mx-auto mb-3">
+                <gala.Image
+                src={i9}
+                className="d-block w-100 imagegala"
+                alt="picture3"
+                fluid
+                />
+            </gala.Col>
+            <gala.Col lg={4} xs={10} className="mx-auto mb-3">
+                <gala.Image
+                src={i10}
+                className="d-block w-100 imagegala"
+                alt="picture3"
+                fluid
+                />
+            </gala.Col>
+        </gala.Row>
+        <gala.Row className="mb-2">
+            <gala.Col lg={4} xs={10} className="mx-auto mb-3">
+                <gala.Image
+                src={i11}
+                className="d-block w-100 imagegala"
+                alt="picture1"
+                fluid
+                />
+            </gala.Col>
+            <gala.Col lg={4} xs={10} className="mx-auto mb-3">
+                <gala.Image
+                src={i12}
+                className="d-block w-100 imagegala"
+                alt="picture2"
+                fluid
+                />
+            </gala.Col>
+            <gala.Col lg={4} xs={10} className="mx-auto mb-3">
+                <gala.Image
+                src={i13}
+                className="d-block w-100 imagegala"
+                alt="picture3"
+                fluid
+                />
+            </gala.Col>
+            
         </gala.Row>
     </gala.Container>
       <div className="ContactSection">
