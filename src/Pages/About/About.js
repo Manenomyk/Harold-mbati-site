0e0f7c849bd50a29f736e9a59a4b7fe7ccec6f62
@Bahatimyk <manenomichael99@gmail.com>
2023-04-02 18:19:12 +0300

    about navbar

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,10 +1,37 @@
 import React from 'react';
-import './Help.css';
-
+import './About.css';
+import * as about from 'react-bootstrap';
 
 function About() {
   return (
-    <div>About</div>
+    <div>
+      <div className="nav_section" id="nav_section">
+        <about.Container className="nav_container">
+          <about.Nav
+            className="justify-content-end"
+            variant="pills"
+            defaultActiveKey="/"
+          >
+            <about.Nav.Item className="link_items">
+              <about.Nav.Link href="#Home">Who We Are</about.Nav.Link>
+            </about.Nav.Item>
+            <about.Nav.Item className="link_items">
+              <about.Nav.Link href="#JoinUs" eventKey="link-1">
+                How To Help
+              </about.Nav.Link>
+            </about.Nav.Item>
+            <about.Nav.Item className="link_items">
+              <about.Nav.Link href="#Contact" eventKey="link-2">
+                Contact Us
+              </about.Nav.Link>
+            </about.Nav.Item>
+            <about.Nav.Item className="link_items">
+              <about.Nav.Link eventKey="link-3">Donate</about.Nav.Link>
+            </about.Nav.Item>
+          </about.Nav>
+        </about.Container>
+      </div>
+    </div>
   )
 }
 
