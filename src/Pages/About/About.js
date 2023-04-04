99c34a554d8b750a9f1cd5850b70fd838a85f427
@Bahatimyk <manenomichael99@gmail.com>
2023-04-04 10:46:20 +0300

    abxout content

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import './About.css';
-import * as about from 'react-bootstrap';
+import React from "react";
+import "./About.css";
+import * as about from "react-bootstrap";
 import logo from "../../Images/logo.png";
-import Contact  from '../Contact/Contact';
+import Contact from "../Contact/Contact";
 import { MdDoubleArrow } from "react-icons/md";
 function About() {
   return (
@@ -35,59 +35,58 @@ function About() {
           </about.Col>
         </about.Row>
       </about.Container>
-      <div className='topimagesect'>
-        <div className='topimage'>
+      <div className="topimagesect">
+        <div className="topimage">
           <about.Container>
-            <div className='abouttopdiv'>
-              <a href="/" className='tohome'>Home <MdDoubleArrow /></a>
+            <div className="abouttopdiv">
+              <a href="/" className="tohome">
+                Home <MdDoubleArrow />
+              </a>
             </div>
-            <div className='aboutcontdiv'>
-              About Us
-            </div>
+            <div className="aboutcontdiv">About Us</div>
           </about.Container>
         </div>
       </div>
 
-    <about.Container>
-      <div className='mx-auto weare'>
-        <div>
-        What we do
-        <div className="underline2 mx-auto"></div>
+      <about.Container>
+        <div className="mx-auto weare">
+          <div>
+            What we do
+            <div className="underline2 mx-auto"></div>
+          </div>
         </div>
-
-      </div>
-      <about.Row>
-        <about.Col lg={3} >
-
-        </about.Col>
-      </about.Row>
-    </about.Container>
-
-
-
-
-      <about.Container className='aboutcontain'>
         <about.Row>
-          <about.Col lg={5} className='mx-auto'>
-            <about.Image 
-            src={logo} 
-            className='d-block w-100'
-            fluid
-            alt='logo image'
+          <about.Col lg={3}></about.Col>
+        </about.Row>
+      </about.Container>
+
+      <about.Container className="aboutcontain">
+        <about.Row>
+          <about.Col lg={5} className="mx-auto">
+            <about.Image
+              src={logo}
+              className="d-block w-100"
+              fluid
+              alt="logo image"
             />
           </about.Col>
-          <about.Col lg={5} className='mx-auto'>
-          <div>
-                <div className="about">About Us</div>
-                <div className="underline1"></div>
-                
-                <div className="conten">
-                  We are a not-for-profit organization implementing development
-                  programs in Kenya bringing together human, financial and
-                  technical resources to address some of the challenges faced by
-                  the poorest and most marginalized communities.
-                </div>
+          <about.Col lg={5} className="mx-auto">
+            <div>
+              <div className="about">About Us</div>
+              <div className="underline3"></div>
+
+              <div className="conten">
+                Harold Mbati Foundation is a private, not-for-profit
+                organization established in 2023 by Mr. Harold Mbati. HMF is
+                implementing development programs in Kenya bringing together
+                human, financial and technical resources to address some of the
+                challenges faced by the poorest and most marginalized
+                communities. Our focus is on food, health, water, quality
+                education, equality and economic opportunities. We invest in
+                human potential, expanding opportunities and improving quality
+                of life.
               </div>
+            </div>
           </about.Col>
         </about.Row>
       </about.Container>
@@ -97,7 +96,7 @@ function About() {
         <Contact />
       </div>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
