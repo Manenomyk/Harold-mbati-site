7ff5850b9f0f973df3240e4a030f1bab6b4a7134
@Bahatimyk <manenomichael99@gmail.com>
2023-04-05 13:33:43 +0300

    page routing

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import Donate from './Pages/Donate/Donate';
 import Mission from'./Pages/Mission/Mission';
 import About from './Pages/About/About';
 import Gallery from './Pages/Gallery/Gallery';
+
 function App() {
   return (
 
@@ -16,6 +17,7 @@ function App() {
         <Route path='/mission' element={<Mission/>} />
         <Route path='/about' element={<About/>} />
         <Route path='/gallery' element={<Gallery/>} />
+        <Route path='/donate' element={<Donate/>} />
       </Routes>
     </Router>
   );
