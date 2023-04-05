cda35f37019681f7a4a9413d8aa4c63895b0d242
@Bahatimyk <manenomichael99@gmail.com>
2023-04-05 13:38:35 +0300

    page nav bar

--- src/Pages/Donate/Donate.js
+++ src/Pages/Donate/Donate.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import * as donate from "react-bootstrap"
 
 function Donate() {
   return (
@@ -13,6 +14,26 @@ function Donate() {
           content="Awesome resort you need to checkout"
         />
       </Helmet>
+      <div className="nav_section" id="nav_section">
+        <donate.Container className="nav_container">
+          <donate.Nav
+            className="justify-content-end"
+            variant="pills"
+            defaultActiveKey="/"
+          >
+            <donate.Nav.Item className="link_items">
+              <donate.Nav.Link id="linkitems" href="/">
+                Home
+              </donate.Nav.Link>
+            </donate.Nav.Item>
+            <donate.Nav.Item className="link_items">
+              <donate.Nav.Link id="linkitems" href="#Contact" eventKey="link-2">
+                Contact Us
+              </donate.Nav.Link>
+            </donate.Nav.Item>
+          </donate.Nav>
+        </donate.Container>
+      </div>
     </div>
   )
 }
