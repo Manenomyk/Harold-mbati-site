16808e0522945fcb1705cb58581093968b5161f0
@Bahatimyk <manenomichael99@gmail.com>
2023-04-12 13:54:44 +0300

    modal function call

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -34,6 +34,16 @@ function MyVerticallyCenteredModal(props) {
 }
 
 function Home() {
+  document.addEventListener("click", function (e) {
+    if (e.target.classList.contains("imagegala")) {
+      const src = e.target.getAttribute("src");
+      console.log(src);
+      // document.querySelector(".ala").src = src;
+      // const myModal = new gala.Modal(document.getElementById('imagemodal'));
+      // myModal.show();
+    }
+  });
+  const [modalShow, setModalShow] = React.useState(false);
   return (
     <div>
       <Helmet>
